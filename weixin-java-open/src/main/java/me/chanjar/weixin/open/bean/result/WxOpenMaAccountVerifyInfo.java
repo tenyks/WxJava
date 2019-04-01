37187aa43dfa0ee9629e2d8c82c2066a7f42b854
tenyks <tenyks@163.com>
2019-04-01 17:24:19 +0800

    修正字段类型

--- weixin-java-open/src/main/java/me/chanjar/weixin/open/bean/result/WxOpenMaAccountVerifyInfo.java
+++ weixin-java-open/src/main/java/me/chanjar/weixin/open/bean/result/WxOpenMaAccountVerifyInfo.java
@@ -21,19 +21,19 @@ public class WxOpenMaAccountVerifyInfo implements Serializable {
    * 是否资质认证（1：是，0：否）若是，拥有微信认证相关的权限。
    */
   @SerializedName("qualification_verify")
-  private Integer  qualificationVerify;
+  private Boolean  qualificationVerify;
 
   /**
    * 是否名称认证（1：是，0：否）对于公众号（订阅号、服务号），是名称认证，微信客户端才会有微信认证打勾的标识。
    */
   @SerializedName("naming_verify")
-  private Integer  namingVerify;
+  private Boolean  namingVerify;
 
   /**
    * 是否需要年审（true：是，false：否）（qualification_verify = true时才有该字段）
    */
   @SerializedName("annual_review")
-  private Integer annualReview;
+  private Boolean annualReview;
 
   /**
    * 年审开始时间，时间戳（qualification_verify = true时才有该字段）
